471aab1f997524980fe150e59a9034607fa9a245
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-04-09 15:20:35 +0800

    refactoring: budget controller test

--- src/test/java/com/odde/bbuddy/budget/BudgetControllerTest.java
+++ src/test/java/com/odde/bbuddy/budget/BudgetControllerTest.java
@@ -4,17 +4,19 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.springframework.ui.Model;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentCaptor.forClass;
-import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.*;
 
 public class BudgetControllerTest {
 
+    private static final String MONTH = "2018-05";
+    private static final String AMOUNT = "1500";
+    private static final String EXISTING_AMOUNT = "1000";
+    private static final int ID = 100;
     BudgetRepo mockBudgetRepo = mock(BudgetRepo.class);
     BudgetController controller = new BudgetController(mockBudgetRepo);
     Budget budget = new Budget();
@@ -22,53 +24,68 @@ public class BudgetControllerTest {
 
     @Test
     public void submit_add_budget_should_go_to_list() {
-        assertThat(controller.submitAddBudget(budget, mockModel)).isEqualTo("/budgets/index");
+        assertThat(addBudget(budget)).isEqualTo("/budgets/index");
     }
     
     @Test
-    public void submit_add_budget_should_call_repo_to_save() {
-        when(mockBudgetRepo.findBudgetByMonthEquals(anyString())).thenReturn(new ArrayList<Budget>());
+    public void add_a_new_budget() {
+        givenExistingBudget();
 
-        budget.setMonth("2018-05");
-        budget.setAmount("1500");
-        controller.submitAddBudget(this.budget, mockModel);
+        addBudget(budget(MONTH, AMOUNT));
 
-        ArgumentCaptor<Budget> captor = ArgumentCaptor.forClass(Budget.class);
-        verify(mockBudgetRepo).save(captor.capture());
-        assertThat(captor.getValue().getMonth()).isEqualTo("2018-05");
-        assertThat(captor.getValue().getAmount()).isEqualTo("1500");
-        assertThat(captor.getValue().getId()).isEqualTo(0);
+        verifySaveBudget(0, MONTH, AMOUNT);
     }
+
     @Test
-    public void update_exists_budget() {
-        Budget existingBudget = new Budget();
-        existingBudget.setId(100);
-        existingBudget.setMonth("2018-05");
-        existingBudget.setAmount("1000");
-        when(mockBudgetRepo.findBudgetByMonthEquals(anyString())).thenReturn(Arrays.asList(existingBudget));
+    public void update_an_exists_budget() {
+        givenExistingBudget(budget(ID, MONTH, EXISTING_AMOUNT));
 
-        budget.setMonth("2018-05");
-        budget.setAmount("1500");
-        controller.submitAddBudget(this.budget, mockModel);
+        addBudget(budget(MONTH, AMOUNT));
 
-        ArgumentCaptor<Budget> captor = ArgumentCaptor.forClass(Budget.class);
-        verify(mockBudgetRepo).save(captor.capture());
-        assertThat(captor.getValue().getMonth()).isEqualTo("2018-05");
-        assertThat(captor.getValue().getAmount()).isEqualTo("1500");
-        assertThat(captor.getValue().getId()).isEqualTo(100);
+        verifySaveBudget(ID, MONTH, AMOUNT);
     }
-
 
     @Test
     public void submit_add_budget_should_set_budget_list_for_view() {
         Budget budget = new Budget();
         when(mockBudgetRepo.findAll()).thenReturn(Arrays.asList(budget));
 
-        controller.submitAddBudget(this.budget, mockModel);
+        addBudget(this.budget);
 
         ArgumentCaptor<List> captor = forClass(List.class);
         verify(mockModel).addAttribute(eq("budgets"), captor.capture());
         assertThat(captor.getValue()).containsExactly(budget);
+    }
+
+    private String addBudget(Budget budget) {
+        return controller.submitAddBudget(budget, mockModel);
+    }
+
+    private void verifySaveBudget(int id, String month, String amount) {
+        ArgumentCaptor<Budget> captor = forClass(Budget.class);
+        verify(mockBudgetRepo).save(captor.capture());
+        assertThat(captor.getValue().getMonth()).isEqualTo(month);
+        assertThat(captor.getValue().getAmount()).isEqualTo(amount);
+        assertThat(captor.getValue().getId()).isEqualTo(id);
+    }
+
+    private void givenExistingBudget(Budget... budgets) {
+        when(mockBudgetRepo.findBudgetByMonthEquals(anyString())).thenReturn(Arrays.asList(budgets));
+    }
+
+    private Budget budget(String month, String amount) {
+        Budget budget = new Budget();
+        budget.setMonth(month);
+        budget.setAmount(amount);
+        return budget;
+    }
+
+    private Budget budget(int id, String month, String amount) {
+        Budget existingBudget = new Budget();
+        existingBudget.setId(id);
+        existingBudget.setMonth(month);
+        existingBudget.setAmount(amount);
+        return existingBudget;
     }
 
 }
